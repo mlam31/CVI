d038ae4c9e91ba266fee3dfa8f988d33313f97a9
uvsq22004307 <mathieu.lam31@gmail.com>
2024-03-26 16:00:23 +0100

    affichage des images de la firebase cloud storage sous forme de grille d'images

---
+++ app/src/main/java/com/carnetdevoyageintelligent/cvi/ImageGridAdapter.kt
@@ -0,0 +1,35 @@
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.carnetdevoyageintelligent.cvi.R
+import com.squareup.picasso.Picasso
+
+class ImageGridAdapter(private val context: Context, private val imageUrls: MutableList<String>) :
+    RecyclerView.Adapter<ImageGridAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.grid_item_image, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val imageUrl = imageUrls[position]
+        Picasso.get().load(imageUrl).into(holder.imageView)
+    }
+
+    override fun getItemCount(): Int {
+        return imageUrls.size
+    }
+
+    fun addImageUrl(imageUrl: String) {
+        imageUrls.add(imageUrl)
+        notifyItemInserted(0)
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.image_view)
+    }
+}
