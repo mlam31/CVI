968c51e8c1a3dbdbd7a82daad983ffb873f0cf04
uvsq22004307 <mathieu.lam31@gmail.com>
2024-04-18 01:58:01 +0200

    map centré sur paris à faire: implémenter les différentes options des voyages + bdd photos données gps

--- app/src/main/java/com/carnetdevoyageintelligent/cvi/Map.kt
+++ app/src/main/java/com/carnetdevoyageintelligent/cvi/Map.kt
@@ -5,28 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.PreferenceManager
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.views.MapView
+import org.osmdroid.api.IGeoPoint
+import org.osmdroid.util.GeoPoint
 
 
-import com.carnetdevoyageintelligent.cvi.R
-
-
-class Map : Fragment() {
-
+class Map: Fragment() {
 
     private lateinit var mapView: MapView
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Initialize the map view
         val rootView = inflater.inflate(R.layout.fragment_map, container, false)
         mapView = rootView.findViewById(R.id.mapView)
-        mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE)
-        mapView.setMultiTouchControls(true)
+
+        // Initialize osmdroid
+        Configuration.getInstance().load(
+            requireContext(),
+            PreferenceManager.getDefaultSharedPreferences(requireContext())
+        )
+
+        // Set the center of the map to the world center (latitude: 0, longitude: 0)
+        val latitudeParis = 48.8566
+        val longitudeParis = 2.3522
+        val geoPoint = GeoPoint(latitudeParis, longitudeParis)
+        val parisGeoPoint : IGeoPoint = geoPoint
+        mapView.controller.setCenter(parisGeoPoint)
+
+        // Set an appropriate zoom level to display the entire world
+        mapView.controller.setZoom(13) // Zoom level 1 usually displays the entire world
+
+        // Set the tile source (e.g., Mapnik)
+        mapView.setTileSource(TileSourceFactory.MAPNIK)
+
         return rootView
     }
+
 }
